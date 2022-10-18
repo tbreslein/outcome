8aaec40ca7b9f9d70d388ddb77c7ac0c402d4b94
tbreslein <tommy.breslein@protonmail.com>
2022-10-18 16:23:03 +0200

    update flake.nix

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "Small lib for a type similar to Rust's Result";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShell = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            cmake
+            ninja
+            pkg-config
+            gcc
+            conan
+
+            cppcheck
+            clang-tools
+            valgrind
+            # ccls
+            cmake-format
+            cmake-language-server
+          ];
+          buildInputs = [ ];
+        };
+      });
+}
