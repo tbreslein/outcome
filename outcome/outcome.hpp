5291615bd5a31aac5cb0d8220efbed61fd927ef0
tbreslein <tommy.breslein@protonmail.com>
2022-11-21 09:13:15 +0100

    add ErrorReport

--- outcome/outcome.hpp
+++ outcome/outcome.hpp
@@ -42,8 +42,7 @@ namespace outcome {
  */
 template <class T, class E>
 class [[nodiscard]] Outcome {
-    /// @brief The std::variant<T,E> that stores either a value or an error.
-    std::variant<T, E> _either;
+    std::variant<T, E> _either; ///< @brief The std::variant<T,E> that stores either a value or an error.
 
   public:
     /// @brief Constructs an outcome::Outcome<std::nullopt_t, E>.
@@ -81,8 +80,8 @@ class [[nodiscard]] Outcome {
  */
 template <class E>
 class [[nodiscard]] Outcome<void, E> {
-    /// @brief The std::variant<std::nullopt_t,E> that stores either a std::nullopt or an error.
-    std::variant<std::nullopt_t, E> _either;
+    std::variant<std::nullopt_t, E>
+        _either; ///< @brief The std::variant<std::nullopt_t,E> that stores either a std::nullopt or an error.
 
   public:
     /// @brief Constructs an outcome::Outcome<std::nullopt_t, E>.
@@ -103,5 +102,119 @@ class [[nodiscard]] Outcome<void, E> {
     /// @brief Retrieves the error
     auto error() const noexcept -> E { return std::get<E>(this->_either); }
 };
+
+#ifdef OUTCOME_USE_ERRORREPORTS
+/**
+ * @brief Wrapper around the kind of error that happened
+ */
+struct ErrorReport {
+    const int code;                ///< Integer error code that should identify the kind of error that happened
+    const std::string description; ///< Description of the error
+    const std::string file;        ///< The source code file where the error occurred
+    const int line;                ///< The line in the source code file where the error occurred
+    const std::string message;     ///< The error message that can be displayed to the user
+
+    /**
+     * @brief Constructs an outcome::ErrorReport
+     * @param code The error code
+     * @param description Description of the error that occured
+     * @param file The source file where the error occurred
+     * @param line The line in that file where the error occurred
+     */
+    explicit ErrorReport(const int code, const std::string &description, const std::string &file, const int line)
+        : code{code}
+        , description{description}
+        , file{file}
+        , line{line}
+        , message{"Error Code "s + to_string(this->code) + "\n  File: "s + this->file + "\n  Line: "s +
+                  to_string(this->line) + "\n  Description: "s + this->description} {}
+};
+
+/**
+ * @brief Convenience using declaration that nails the error type to an outcome::ErrorReport
+ * @tparam T The value type for outcome::Outcome
+ */
+template <class T>
+using Outcome = Outcome<T, ErrorReport>;
+
+#endif // OUTCOME_USE_ERRORREPORTS
+
+#ifdef OUTCOME_USE_MACROS
+
+/**
+ * @brief Checks the condition @p cond, and returns the enclosing scope with @p err if the condition does not hold.
+ * @param condition The condition that is going to be checked
+ * @param error The error that is going to be returned
+ *
+ * NOTE: Your condition should be written such that not passing the check is unlikely, since the code path where the
+ * condition does NOT hold, is annotated with an `[[unlikely]]`.
+ *
+ * Example:
+ *     // Assume that we use outcome::ErrorReport as the error type.
+ *
+ *     auto foo() noexcept -> outcome::Outcome<int, outcome::ErrorReport> {
+ *         // This would pass and this not disrupt the function flow
+ *         const int i {2};
+ *         OUTCOME_ENSURE(i > 1, outcome::ErrorReport(101, "i must be greater than 1!", __FILE__, __LINE__));
+ *
+ *         // This condition would not pass, so the outcome::ErrorReport is returned.
+ *         OUTCOME_ENSURE(i % 2 != 0, outcome::ErrorReport(102, "i must be uneven!", __FILE__, __LINE__));
+ *
+ *         // This part of the code is never reached, because the previous OUTCOME_ENSURE call made the function return
+ *         // early.
+ *
+ *         return i+1;
+ *     }
+ */
+#define OUTCOME_ENSURE(condition, error)                                                                               \
+    if (!(condition)) [[unlikely]] {                                                                                   \
+        return {error};                                                                                                \
+    }
+
+/**
+ * @brief Takes the value @p val, and returns the error from the enclosing scope of this macro call, if the value
+ * contains an error.
+ * @p val The value to check
+ *
+ * NOTE: The code path where the value DOES contain an error, is annotated with an `[[unlikely]]`.
+ *
+ * Example:
+ *     // Assume that we use outcome::ErrorReport as the error type.
+ *
+ *     // This function prints its argument, but only if that argument is even; otherwise it returns an error.
+ *     auto print_if_even(const int i) noexcept -> outcome::Outcome<void, outcome::ErrorReport> {
+ *         OUTCOME_ENSURE(i % 2 == 0, outcome::ErrorReport(101, "i must be even!", __FILE__, __LINE__));
+ *         printf("i = %d\n", i);
+ *         return {};
+ *     }
+ *
+ *     // This function prints its argument, but only if that argument is uneven; otherwise it returns an error.
+ *     auto print_if_uneven(const int i) noexcept -> outcome::Outcome<void, outcome::ErrorReport> {
+ *         OUTCOME_ENSURE(i % 2 != 0, outcome::ErrorReport(102, "i must be uneven!", __FILE__, __LINE__));
+ *         printf("i = %d\n", i);
+ *         return {};
+ *     }
+ *
+ *     auto foo() -> outcome::Outcome<void, outcome::ErrorReport> {
+ *         const int i {2};
+ *
+ *         // This function call would just pass since i is indeed even, so we see the terminal output.
+ *         OUTCOME_UNWRAP(print_if_even(2));
+ *
+ *         // This function call would NOT pass, so the macro returns the outcome::ErrorReport described in the
+ *         // function.
+ *         OUTCOME_UNWRAP(print_if_uneven(2));
+ *
+ *         // This call is never reached, since the previous OUTCOME_UNWRAP call had the function return early.
+ *         printf("I am never reached\n");
+ *         return {};
+ *     }
+ */
+#define OUTCOME_UNWRAP(val)                                                                                            \
+    if (const auto err{val}; err.has_error()) [[unlikely]] {                                                           \
+        return {err.error()};                                                                                          \
+    }
+
+#endif // OUTCOME_USE_MACROS
 
 } // namespace outcome
